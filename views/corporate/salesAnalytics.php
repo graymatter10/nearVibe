5e0c3121ff5038ec45df236ce8165781fbe2b0bb
no name <noname@noname.net>
2026-09-10 19:40:02 +0600

    added partials and template pages for corporate

---
+++ views/corporate/salesAnalytics.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php require("../../partials/corporate/head.php") ?>
+</head>
+<body>
+<?php require("../../partials/corporate/header.php") ?>
+
+This is Sales Analytics
+
+<?php require("../../partials/corporate/header-end.php") ?>
+
+</body>
+</html>
