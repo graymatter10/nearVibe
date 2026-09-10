75d1b5476f0ecd8ae17e8c9884b7aba87e3fbad4
no name <noname@noname.net>
2026-09-10 19:26:59 +0600

    added partials and template pages for admin

---
+++ partials/admin/head.php
@@ -0,0 +1,58 @@
+<title>NearVibe</title>
+<style>
+    body {
+        margin: 0;
+        background-color: whitesmoke;
+        font-family: Arial;
+    }
+    #heading {
+        height: 60px;
+        background-color: white;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 25px;
+    }
+    #logo {
+        color: blue;
+    }
+    #admin {
+        background-color: skyblue;
+        color: blue;
+        padding: 6px 15px;
+        border-radius: 20px;
+    }
+    #layout {
+        display: flex;
+    }
+    #menu {
+        background-color: #0d1330;
+        height: 100vh;
+        width: 30vh;
+    }
+    #menu button {
+        width: calc(100% - 20px);
+        margin: 8px 10px;
+        padding: 15px 25px;
+        background-color: #0d1330;
+        border: none;
+        border-radius: 10px;
+        color: #aaa;
+        text-align: left;
+        cursor: pointer;
+    }
+    #menu button:hover {
+        background-color: #1c2450;
+        color: white;
+    }
+    #menu button.active {
+        background-color: #2947a3;
+        color: white;
+    }
+    #menu button.active:hover {
+        background-color: #3a5bc7;
+    }
+    #middle {
+        padding: 20px;
+    }
+</style>
---
+++ views/admin/accountSettings.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php require("../../partials/admin/head.php") ?>
+</head>
+<body>
+<?php require("../../partials/admin/header.php") ?>
+
+This is Account Settings
+
+<?php require("../../partials/admin/header-end.php") ?>
+
+</body>
+</html>
